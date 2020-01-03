2f545e5c9c4b8eedca184323f0f606996f24a47b
Jesus Machado <maxhadortiz93@gmail.com>
2020-01-03 09:49:50 -0400

    V 0.0.3
     Save Data Models  User and Drivers

--- routes/web.php
+++ routes/web.php
@@ -24,45 +24,106 @@ Auth::routes();
 //Route::get('/home', 'HomeController@index')->name('home');
 
 // Lista de Clientes
-Route::get('/users', function (){    
+Route::get('/api/users', function (){    
       return Response::json((App\User::with('phone','location','social')->where('type','cliente')->get()));
 });
 // Lista de Taxistas
-Route::get('/drivers', function (){    
+Route::get('/api/drivers', function (){    
     return Response::json((App\User::with('phone','location','social')->where('type','taxista')->get()));
 });
 
 //Crear el Usuario
-Route::post('/user', function(Request $request){
-    $user = factory(App\User::class)->create();
+Route::post('/api/user', function(Request $request){
+    $user = new App\User;
+    $data = $request->json()->all();
+    
+    $user->type         = $data['type'];
+    $user->name         = $data['name'];
+    $user->apellido     = $data['apellido'];
+    $user->birthdate    = $data['birthdate'];
+    $user->genero       = $data['genero'];
+    $user->cedula       = $data['cedula'];
+    $user->email        = $data['email'];
+    $user->password = Hash::make($data['password']);
+    $user->save();
+    
     //Modelo Phone
     $phone = new App\Phone();
-    $phone->phone_local = '+58 02934331307';
-    $phone->phone_movil = '+58 0412-1866163';
+    $phone->phone_local = $data['phone_local'];
+    $phone->phone_movil = $data['phone_movil'];
     $user->phone()->save($phone);
 
     //Modelo Location
     $location = new App\Location();
-    $location->country = 'Venezuela';
-    $location->city = 'Sucre';
-    $location->location = 'La Granaja, Sector Cantarrana';
-    $location->code_postal = '6101';
+    $location->country      = $data['country'];
+    $location->city         = $data['city'];
+    $location->location     = $data['location'];
+    $location->code_postal  = $data['code_postal'];
     $user->location()->save($location);
-
 
     //Modelo Social
     $social = new App\Social();
-    $social->url = 'https://www.google.co.ve/';
-    $social->act = 'act-1';
-    $social->facebook = 'https://www.facebook.com/';
-    $social->instagram = 'https://www.instagram.com/';
-    $social->twitter = 'https://twitter.com/home';
-    $social->youtube = 'https://www.youtube.com/';
-    $social->pagina_web = 'https://laravel.com/docs/5.8/routing';
-    $social->id_google = 'https://www.google.co.ve/';
-    $social->id_token = 'https://www.google.co.ve/';
-    $social->url_imagen = 'https://placekitten.com/200/300';
+    $social->url        = $data['url'];
+    $social->act        = $data['act'];
+    $social->facebook   = $data['facebook'];
+    $social->instagram  = $data['instagram'];
+    $social->twitter    = $data['twitter'];
+    $social->youtube    = $data['youtube'];
+    $social->pagina_web = $data['pagina_web'];
+    $social->id_google  = $data['id_google'];
+    $social->id_token   = $data['id_token'];
+    $social->url_imagen = $data['url_imagen'];
     $user->social()->save($social);
+    
+
+    return Response::json(['Respuesta' => TRUE], 201);
+});
+
+//Crear el Taxista
+Route::post('/api/driver', function(Request $request){
+    $user = new App\User;
+    $data = $request->json()->all();
+    
+    $user->type         = $data['type'];
+    $user->name         = $data['name'];
+    $user->apellido     = $data['apellido'];
+    $user->birthdate    = $data['birthdate'];
+    $user->genero       = $data['genero'];
+    $user->cedula       = $data['cedula'];
+    $user->email        = $data['email'];
+    $user->password = Hash::make($data['password']);
+    $user->save();
+    
+    //Modelo Phone
+    $phone = new App\Phone();
+    $phone->phone_local = $data['phone_local'];
+    $phone->phone_movil = $data['phone_movil'];
+    $user->phone()->save($phone);
+
+    //Modelo Location
+    $location = new App\Location();
+    $location->country      = $data['country'];
+    $location->city         = $data['city'];
+    $location->location     = $data['location'];
+    $location->code_postal  = $data['code_postal'];
+    $user->location()->save($location);
+
+    //Modelo Social
+    $social = new App\Social();
+    $social->url        = $data['url'];
+    $social->act        = $data['act'];
+    $social->facebook   = $data['facebook'];
+    $social->instagram  = $data['instagram'];
+    $social->twitter    = $data['twitter'];
+    $social->youtube    = $data['youtube'];
+    $social->pagina_web = $data['pagina_web'];
+    $social->id_google  = $data['id_google'];
+    $social->id_token   = $data['id_token'];
+    $social->url_imagen = $data['url_imagen'];
+    $user->social()->save($social);
+    
+
+    return Response::json(['Respuesta' => TRUE], 201);
 });
 
 //Modificar el Usuario
